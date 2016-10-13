9502ad577e119dab7e6d396bdac0c0d155c5f62a
Clément Pasquier <clement@n00sphere.net>
2016-10-13 20:08:44 +0200

    add recipe system

---
+++ plugins/gna.js
@@ -0,0 +1,12 @@
+const internals = {};
+
+internals.GnaPlugin = module.exports = {};
+
+
+internals.GnaPlugin.register = (server, options, next) => {
+
+};
+
+internals.GnaPlugin.register.attributes = {
+  name: 'gna'
+};
