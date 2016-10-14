b776c6cd8b1d04e4b17604c1a5050a7c81d203cb
Clément Pasquier <clement@n00sphere.net>
2016-10-14 18:13:13 +0200

    tmp

--- plugins/gna.js
+++ plugins/gna.js
@@ -5,6 +5,16 @@ internals.GnaPlugin = module.exports = {};
 
 internals.GnaPlugin.register = (server, options, next) => {
 
+
+  server.route({
+    path: '/',
+    method: 'GET',
+    handler : function (request, reply) {
+      reply('ok')
+    }
+  });
+
+  next();
 };
 
 internals.GnaPlugin.register.attributes = {
